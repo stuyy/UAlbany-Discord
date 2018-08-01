2a1aebbe05c2906de8a8c2c05b1f10fe396b2c6f
ansonfoong <afoong@albany.edu>
2018-08-01 04:01:18 -0400

    Bot will search if user has existing roles from json file, if they do, they will not be added to the role

--- bot.js
+++ bot.js
@@ -16,6 +16,41 @@ client.on('message', message => {
   {
     message.reply('Welcome!');
   }
+
+  if(message.content === '!checkrole')
+  {
+    /*
+    role = message.guild.roles.find('name', data.majorRole[0]);
+    console.log("Searching if user has " + data.majorRole[0] + " role");
+    if(message.member.roles.has(role.id))
+    {
+      console.log("hello");
+      message.channel.send(message.author.username +  " has the " + data.majorRole[0] + " role!");
+    }
+
+    console.log("Size of array: " + data.majorRole.length);
+    */
+    var count = 0;
+    var majorArraySize = data.majorRole.length;
+    while(count<majorArraySize)
+    {
+      // find the role first.
+      role = message.guild.roles.find('name', data.majorRole[count]); // search each role
+      console.log("Searching if user has " + data.majorRole[count] + " role");
+      if(role === null)
+      {
+        count++;
+        continue;
+      }
+      else if(message.member.roles.has(role.id))
+      {
+        message.channel.send(message.author.username + " has the " + data.majorRole[count] + " role!");
+      }
+      count++;
+    }
+
+
+  }
   if(message.content === '!csrole')
   {
     var getRole = message.member.guild.roles.find('name', 'Computer Science');
@@ -38,13 +73,17 @@ client.on('message', message => {
     {
       findRole('Computer Science', message);
     }
+    else if(args[1] === 'bio')
+    {
+      findRole('Biology', message);
+    }
   }
 
 });
 
 function findRole(roleName, message)
 {
-  var getRole = message.member.guild.roles.find('name', 'Computer Science');
+  var getRole = message.member.guild.roles.find('name', roleName);
   var roleID = getRole.id;
 
   message.member.addRole(roleID);
