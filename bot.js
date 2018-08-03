0dacef48fc6c9148ea9284bf5eb0ef5f6a3770de
ansonfoong <afoong@albany.edu>
2018-08-03 13:21:41 -0400

    Updating bot.js and json

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const botinfo = require("./botinfo.json");
 const request = require('request');
-const path = require('path');
-const pdf = require('pdf-poppler');
 
 client.login(process.env.BOT_TOKEN);
 
