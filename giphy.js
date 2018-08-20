886faaf590d1969d9bc49e2c89596bbe86f36bdf
ansonfoong <afoong@albany.edu>
2018-08-20 15:55:48 -0400

    Fix

--- giphy.js
+++ giphy.js
@@ -11,7 +11,7 @@ exports.sendGIF = function sendGIF(message, searchQuery)
   // limit is 250, but there may not be 250 results.
   request(url, function(err, res, body){
     var queryData = JSON.parse(body);
-    message.channel.send({files: [new Discord.Attachment(queryData.data.images.downsized_still.url), new Discord.Attachment('./giphy.png')]});
+    message.channel.send({files: [new Discord.Attachment(queryData.data.images.downsized_medium.url), new Discord.Attachment('./giphy.png')]});
   });
 
 }
