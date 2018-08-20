3cd5496cb4a28ce594a553d5578f7e0c7542e252
ansonfoong <afoong@albany.edu>
2018-08-20 14:15:53 -0400

    Changed the search to random

--- giphy.js
+++ giphy.js
@@ -4,7 +4,7 @@ const request = require('request');
 exports.sendGIF = function sendGIF(message, searchQuery)
 {
   var number = Math.floor(Math.random() * 50);
-  var url = 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + searchQuery + '&limit=1&offset=' + number + '&rating=G&lang=en';
+  var url = 'https://api.giphy.com/v1/gifs/random?api_key='+key+'&tag='+searchQuery+'&rating=G';
   console.log(url);
 
   // limit is 250, but there may not be 250 results.
