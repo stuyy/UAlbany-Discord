bf3323c240460c75d4301ba601875a106f6f1473
ansonfoong <afoong@albany.edu>
2018-08-20 13:05:19 -0400

    Changed the random number to be max amount of results found

--- giphy.js
+++ giphy.js
@@ -5,10 +5,14 @@ exports.sendGIF = function sendGIF(message, searchQuery)
 {
   var url = 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + searchQuery + '&limit=250&offset=0&rating=G&lang=en';
   console.log(url);
-  var number = Math.floor(Math.random() * 250);
-  console.log(number);
+
+  // limit is 250, but there may not be 250 results.
   request(url, function(err, res, body){
     var queryData = JSON.parse(body);
+    var resultLength = queryData.data.length;
+    console.log(resultLength + " gifs found");
+    var number = Math.floor(Math.random() * resultLength);
+    console.log(number);
     if(queryData.data.length === 0)
     {
       message.channel.send("No results found.");
