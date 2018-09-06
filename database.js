bfaaccd8c0e99ecfb423bec6e399240a1249a77b
ansonfoong <afoong@albany.edu>
2018-09-06 17:41:46 -0400

    Test

--- database.js
+++ database.js
@@ -23,7 +23,8 @@ exports.showTable = function showTable(message)
 
 exports.viewXP = function viewXP(message)
 {
-  var channel = new Discord.Guild().channels.find(c => c.name === 'xp-levels');
+  var channel = new Discord.Guild();
+  channel = channel.channels.find(c => c.name === 'xp-levels');
   con.query(`SELECT * FROM level WHERE id = ${message.author.id}`, (err, result) => {
     if(err) throw err;
     console.log(message.author + " has " + result[0].xp + " total xp");
