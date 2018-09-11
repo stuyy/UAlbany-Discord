ddf001f5d2d5f175f1388196361dbfc16018f6de
ansonfoong <afoong@albany.edu>
2018-09-11 01:12:02 -0400

    Remove someone from the DB

--- bot.js
+++ bot.js
@@ -125,7 +125,7 @@ client.on('message', message => {
   }
   else if(message.content.toLowerCase() === '!fetch')
   {
-    client.fetchUser('217117467439398912').then(user => console.log(user.username));
+    client.fetchUser('396345752458100748').then(user => console.log(user.username));
   }
   else
   {
