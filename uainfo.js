57e986dad93c770fa0300055fc58f2921b36235e
ansonfoong <afoong@albany.edu>
2018-09-13 03:06:36 -0400

    Test

--- uainfo.js
+++ uainfo.js
@@ -66,10 +66,9 @@ exports.showIntro = function(message)
   if(channel != null)
   {
     const embed = new Discord.RichEmbed()
-    .setTitle(" ")
     .setAuthor("Welcome to the UAlbany Community Discord!", "https://pbs.twimg.com/profile_images/982255592222830592/09swEyNH_400x400.jpg")
     .setDescription("Our goal together is to build an online community for students part of the UAlbany campus to engage, socialize, and get connected with one another. We are NOT affiliated with the school, but our purpose remains to provide server members a convenient and relaxing environment to have open-ended discussions about anything. With that being said, here are some important things you need to know about the community:")
-    .addField(" ", "- UAlbany students, alumni, applicants, faculty, or anyone associated with the University are welcome here. A server member does not necessarily have to be a current student or faculty member, they may have either graduated or may be looking to attend the school and needs some additional information.");
+    .addField("d", "- UAlbany students, alumni, applicants, faculty, or anyone associated with the University are welcome here. A server member does not necessarily have to be a current student or faculty member, they may have either graduated or may be looking to attend the school and needs some additional information.");
     channel.send({embed});
   }
   else {
