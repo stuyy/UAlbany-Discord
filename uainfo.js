1b2b99f1b80bb8a84f211421dee9f4a92ace0afd
ansonfoong <afoong@albany.edu>
2018-09-13 03:10:16 -0400

    Updating Embed

--- uainfo.js
+++ uainfo.js
@@ -68,7 +68,7 @@ exports.showIntro = function(message)
     const embed = new Discord.RichEmbed()
     .setAuthor("Welcome to the UAlbany Community Discord!", "https://pbs.twimg.com/profile_images/982255592222830592/09swEyNH_400x400.jpg")
     .setDescription("Our goal together is to build an online community for students part of the UAlbany campus to engage, socialize, and get connected with one another. We are NOT affiliated with the school, but our purpose remains to provide server members a convenient and relaxing environment to have open-ended discussions about anything.")
-    .addField("With that being said, here are some important things you need to know about the community:", "- UAlbany students, alumni, applicants, faculty, or anyone associated with the University are welcome here. A server member does not necessarily have to be a current student or faculty member, they may have either graduated or may be looking to attend the school and needs some additional information.\n- The server is not restricted to one's age, ethnicity, nationality, religion, culture, sexual orientation, political views, etc.");
+    .addField("With that being said, here are some important things you need to know about the community:", "• UAlbany students, alumni, applicants, faculty, or anyone associated with the University are welcome here. A server member does not necessarily have to be a current student or faculty member, they may have either graduated or may be looking to attend the school and needs some additional information.\n\n• The server is not restricted to one's age, ethnicity, nationality, religion, culture, sexual orientation, political views, etc.");
 
     channel.send({embed});
   }
